ef3bec43d395642e50d6107f3e7e466ed812e8cb
Jan Capdevila <65243539+Goan125@users.noreply.github.com>
2024-05-18 13:12:26 +0200

    ataques

---
+++ Assets/Scripts/PlayerSlash.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSlash : MonoBehaviour
+{
+    [SerializeField] private Transform controladorGolpe;
+    [SerializeField] private float radioGolpe;
+    [SerializeField] private float dañoGolpe;
+    private Animator animator;
+
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Golpe();
+        }
+    }
+    
+    
+    
+    private void Golpe()
+    {
+        animator.SetTrigger("Attack");
+        
+        Collider2D[] objetos = Physics2D.OverlapCircleAll (controladorGolpe.position, radioGolpe);
+
+        foreach (Collider2D colisionador in objetos)
+        {
+            if (colisionador.CompareTag("Enemigo"))
+            {
+                colisionador.transform.GetComponent<Enemigo>().TomarDaño(dañoGolpe);
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+    }
+
+}
